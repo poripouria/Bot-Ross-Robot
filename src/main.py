cc2b3f3e09ff1be77b85f88d0b7582f0da7ae6f5
Poripouria <poripouria9@gmail.com>
2024-01-29 10:52:16 +0330

    add cmd logger

--- src/main.py
+++ src/main.py
@@ -10,51 +10,54 @@ class BotRoss():
     def __init__(self):
         pos = (0, 0, 0)
         
-def cmd_sender(logger='./logs/painting-simulator-logger.txt'):
+def cmd_sender(simulator='./logs/painting-simulator-logger.txt'):
     """
     Generator function to read the logger file and yield the commands for ino server
     """
     pattern = re.compile(r' fr \((\d+), (\d+)\) to \((\d+), (\d+)\)')
 
-    with open(logger, 'r') as file:
+    with open(simulator, 'r') as file:
         lines = file.readlines()
 
-    for line in lines:
-        cmd = ""
-        if line.startswith('Painting Segment'):
-            continue
-        if line.startswith('EOF'):
-            yield '000'
-            break
-        elif line.startswith('Mv') or line.startswith('Dr'):
+    with open('./logs/commands-logger.txt', 'w') as f:
+        for line in lines:
+            cmd = ""
+            if line.startswith('Painting Segment'):
+                continue
+            if line.startswith('EOF'):
+                f.write('000' + '\n')
+                yield '000'
+                break
+            elif line.startswith('Mv') or line.startswith('Dr'):
 
-            if line.startswith('Dr'):
-                cmd += '1'
-            if line.startswith('Mv'):
-                cmd += '0'
+                if line.startswith('Dr'):
+                    cmd += '1'
+                if line.startswith('Mv'):
+                    cmd += '0'
 
-            x1, y1, x2, y2 = map(int, pattern.search(line).groups())
-            # print(x1, x2, y1, y2)
-            dx = x2 - x1
-            dy = y2 - y1
-            if dx < 0:
-                cmd += '+'
-            elif dx > 0:
-                cmd += '-'
+                x1, y1, x2, y2 = map(int, pattern.search(line).groups())
+                # print(x1, x2, y1, y2)
+                dx = x2 - x1
+                dy = y2 - y1
+                if dx < 0:
+                    cmd += '+'
+                elif dx > 0:
+                    cmd += '-'
+                else:
+                    cmd += '0'
+                if dy < 0:
+                    cmd += '+'
+                elif dy > 0:
+                    cmd += '-'
+                else:
+                    cmd += '0'
             else:
-                cmd += '0'
-            if dy < 0:
-                cmd += '+'
-            elif dy > 0:
-                cmd += '-'
-            else:
-                cmd += '0'
-        else:
-            raise ValueError("Something went wrong in Logger file!")
+                raise ValueError("Something went wrong in simulator file!")
 
-        milliseconds = 100
-        time.sleep(milliseconds / 1000.0)
-        yield cmd
+            # milliseconds = 100
+            # time.sleep(milliseconds / 1000.0)
+            f.write(cmd + '\n')
+            yield cmd
         
 def main(Args=None):
     # test_image = cv2.imread("./assets/images/test/image.png")
