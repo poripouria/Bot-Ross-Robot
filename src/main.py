f0856e578c229451b3b8e1e28c3ff7f13daac575
Poripouria <poripouria9@gmail.com>
2024-01-29 11:20:51 +0330

    edit command-logger style for c++ array

--- src/main.py
+++ src/main.py
@@ -19,45 +19,42 @@ def cmd_sender(simulator='./logs/painting-simulator-logger.txt'):
     with open(simulator, 'r') as file:
         lines = file.readlines()
 
-    with open('./logs/commands-logger.txt', 'w') as f:
-        for line in lines:
-            cmd = ""
-            if line.startswith('Painting Segment'):
-                continue
-            if line.startswith('EOF'):
-                f.write('000' + '\n')
-                yield '000'
-                break
-            elif line.startswith('Mv') or line.startswith('Dr'):
+    for line in lines:
+        cmd = ""
+        if line.startswith('Painting Segment'):
+            continue
+        if line.startswith('EOF'):
+            yield "000"
+            break
+        elif line.startswith('Mv') or line.startswith('Dr'):
 
-                if line.startswith('Dr'):
-                    cmd += '1'
-                if line.startswith('Mv'):
-                    cmd += '0'
+            if line.startswith('Dr'):
+                cmd += "1"
+            if line.startswith('Mv'):
+                cmd += "0"
 
-                x1, y1, x2, y2 = map(int, pattern.search(line).groups())
-                # print(x1, x2, y1, y2)
-                dx = x2 - x1
-                dy = y2 - y1
-                if dx < 0:
-                    cmd += '+'
-                elif dx > 0:
-                    cmd += '-'
-                else:
-                    cmd += '0'
-                if dy < 0:
-                    cmd += '+'
-                elif dy > 0:
-                    cmd += '-'
-                else:
-                    cmd += '0'
+            x1, y1, x2, y2 = map(int, pattern.search(line).groups())
+            # print(x1, x2, y1, y2)
+            dx = x2 - x1
+            dy = y2 - y1
+            if dx < 0:
+                cmd += "+"
+            elif dx > 0:
+                cmd += "-"
             else:
-                raise ValueError("Something went wrong in simulator file!")
+                cmd += "0"
+            if dy < 0:
+                cmd += "+"
+            elif dy > 0:
+                cmd += "-"
+            else:
+                cmd += "0"
+        else:
+            raise ValueError("Something went wrong in simulator file!")
 
-            # milliseconds = 100
-            # time.sleep(milliseconds / 1000.0)
-            f.write(cmd + '\n')
-            yield cmd
+        # milliseconds = 100
+        # time.sleep(milliseconds / 1000.0)
+        yield cmd
         
 def main(Args=None):
     test_image = cv2.imread("./assets/images/test/circle-test.png")
@@ -110,6 +107,13 @@ def main(Args=None):
     algorithm(test_image)
     commands = list(cmd_sender())
     # print(commands)
+    with open('./logs/commands-logger.txt', 'w') as f:
+        f.write('char str[n][4] = {')
+        cntr = 0
+        for i, cmd in enumerate(commands):
+            f.write(f'\"{cmd}\", ') if i < len(commands)-1 else f.write(f'\"{cmd}\"' + '};')
+            cntr += 1
+        f.write("\n" + str(cntr))
 
     #TODO: Add Pruning function to Prune the Graph which is extracted feom binary image
 
