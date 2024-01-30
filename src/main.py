fix scaling bugs

src/main.py
@@ -83,7 +83,7 @@ def cmd_sender(commands):
 
 
 def main(Args=None):
-    test_image = cv2.imread("./assets/images/logo/Bot-Ross Logo-02.png")
+    test_image = cv2.imread("./assets/images/test/full-circle.png")
     # test_image = text_to_image("K-P")
     # test_image = np.array([[0, 1, 0, 1, 0, 1],
     #                        [0, 0, 0, 1, 0, 1],